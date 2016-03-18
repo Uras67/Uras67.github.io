98fab58dc11f6118325020fe6227af1a01555eda
Uras67 <uraspehlivanoglu@gmail.com>
2016-03-18 10:31:40 +0200

    18/03/2016
    -Logic operators

---
+++ forc.js
@@ -0,0 +1,29 @@
+var timer;
+var elemID = "#disp";
+var id=0;
+
+
+
+
+$().ready(function() {
+    var top;
+    var left;
+    for(var i=0; i<10; i++) {
+        left =Math.floor((Math.random() * 1000) + 1);
+        top =Math.floor((Math.random() * 800) + 1);
+        $(elemID).append(
+         "<div id='"+ i + "' class='b' style='top:"
+         + top + "px; left:" +left + "px;'>" +
+         i + "</div>");
+    }
+    timer = setInterval(function() {
+        left =Math.floor((Math.random() * 1000) + 1);
+        top =Math.floor((Math.random() * 800) + 1);
+        $("#5").css("left",left+"px");
+        $("#5").css("top", top+"px");
+        $("#5").html(id++);
+    },2000);
+});
+
+
+
---
+++ nand.js
@@ -0,0 +1,19 @@
+function and(a,b) {
+     return (a && b);
+                }
+                
+                function not (a) {
+                    return !a;
+                }
+                
+                function nand
+                
+$().ready(function() {
+      var r = orwithnanda(false,false);
+      $('#res').append("0,0 -> " + r+ "<br/>");
+      r= orwithnanda(false,true);
+      $('#res').append("0,1 -> " + r+ "<br/>");
+      r = orwithnanda(true,false);
+      $('#res').append("1,0 -> " + r+ "<br/>");
+      r = orwithnanda(true,true);
+      $('#res').append("1,1 -> " + r+ "<br/>");
